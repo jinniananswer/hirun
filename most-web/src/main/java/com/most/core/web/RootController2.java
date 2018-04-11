ef6f75141394ed449e95c7af5aed50b82a562feb
awx0415 <awx0415@163.com>
2018-04-11 17:12:25 +0800

    提交代码

---
+++ most-web/src/main/java/com/most/core/web/RootController2.java
@@ -0,0 +1,15 @@
+package com.most.core.web;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @Author anwx
+ * @Date 2018/1/23 22:12
+ * @Description:
+ */
+public class RootController2 {
+
+    protected Logger logger = LogManager.getLogger(this.getClass().getName());
+
+}
