d01f0684020aeefaaa24645c411e28d7ff097eb8
jinnian <38103152@qq.com>
2018-04-12 22:02:27 +0800

    startup

--- most-app/src/main/java/com/most/core/app/init/AppBootupServlet.java
+++ most-app/src/main/java/com/most/core/app/init/AppBootupServlet.java
@@ -38,7 +38,7 @@ public class AppBootupServlet extends GenericServlet{
 
 
         try {
-            testQuery("5");
+            this.testDelete();
         } catch (SQLException e) {
             e.printStackTrace();
             log.error(e);
@@ -53,6 +53,51 @@ public class AppBootupServlet extends GenericServlet{
         param.put("TYPE",type);
         RecordSet result = dao.query("sys_static_data", param);
         log.debug("result==============="+result.get(0, "CREATE_DATE"));
+    }
+
+    private void testInsert() throws SQLException{
+        ConnectionWrapper conn = ConnectionFactory.getConnection("hirun_sys");
+        GenericDAO dao = new GenericDAO(conn);
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("DATA_ID","5");
+        param.put("TYPE","6");
+        param.put("VALUE","7");
+        param.put("PARENT_DATA_ID","2");
+        param.put("CREATE_DATE","2018-04-11 16:41:00");
+        int num = dao.insert("sys_static_data", param);
+        log.debug("*******插入数据******"+num+"条");
+    }
+
+    private void testUpdate() throws SQLException{
+        ConnectionWrapper conn = ConnectionFactory.getConnection("hirun_sys");
+        GenericDAO dao = new GenericDAO(conn);
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("DATA_ID","5");
+        param.put("TYPE","7");
+        param.put("VALUE","8");
+        param.put("PARENT_DATA_ID","9");
+        param.put("CREATE_DATE","2018-04-10 16:41:00");
+        int num = dao.update("sys_static_data", param);
+        log.debug("*******更新数据******"+num+"条");
+    }
+
+    private void testSave() throws SQLException{
+        ConnectionWrapper conn = ConnectionFactory.getConnection("hirun_sys");
+        GenericDAO dao = new GenericDAO(conn);
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("DATA_ID","5");
+        param.put("CREATE_DATE","2018-04-12 16:02:00");
+        int num = dao.save("sys_static_data", param);
+        log.debug("*******更新数据******"+num+"条");
+    }
+
+    private void testDelete() throws SQLException{
+        ConnectionWrapper conn = ConnectionFactory.getConnection("hirun_sys");
+        GenericDAO dao = new GenericDAO(conn);
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("DATA_ID","5");
+        int num = dao.delete("sys_static_data", param);
+        log.debug("*******删除数据******"+num+"条");
     }
 
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
