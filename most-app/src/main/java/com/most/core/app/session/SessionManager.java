4da5c5aec1086a2f0a76ef5636f8e17cad839321
jinnian <38103152@qq.com>
2018-04-16 11:25:39 +0800

    startup

---
+++ most-app/src/main/java/com/most/core/app/session/SessionManager.java
@@ -0,0 +1,25 @@
+package com.most.core.app.session;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @Author jinnian
+ * @Date 2018/4/16 11:20
+ * @Description:
+ */
+public class SessionManager {
+
+    private static Logger log = LogManager.getLogger(SessionManager.class.getName());
+
+    private static final ThreadLocal<AppSession> threadSession = new ThreadLocal<AppSession>();
+
+    public static AppSession getSession(){
+        AppSession session = threadSession.get();
+        if(session == null){
+            session = new AppSession();
+            threadSession.set(session);
+        }
+        return session;
+    }
+}
