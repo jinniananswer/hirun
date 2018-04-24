420eab7bcbddb37d2279b72d446580af4c49c989
jinnian <38103152@qq.com>
2018-04-24 11:53:54 +0800

    startup

---
+++ src/main/java/com/hirun/app/dao/employee/EmployeeJobRoleDAO.java
@@ -0,0 +1,27 @@
+package com.hirun.app.dao.employee;
+
+import com.hirun.pub.domain.entity.org.EmployeeJobRoleEntity;
+import com.most.core.app.database.dao.StrongObjectDAO;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author jinnian
+ * @Date 2018/4/23 22:56
+ * @Description:
+ */
+public class EmployeeJobRoleDAO extends StrongObjectDAO {
+
+    public EmployeeJobRoleDAO(String databaseName){
+        super(databaseName);
+    }
+
+    public List<EmployeeJobRoleEntity> queryJobRoleByEmployeeId(String employeeId) throws SQLException{
+        Map<String, String> parameter = new HashMap<String, String>();
+        parameter.put("EMPLOYEE_ID", employeeId);
+        return this.query(EmployeeJobRoleEntity.class, "ins_employee_job_role", parameter);
+    }
+}
--- src/main/java/com/hirun/web/biz/HomeController.java
+++ src/main/java/com/hirun/web/biz/HomeController.java
@@ -1,25 +1,90 @@
 package com.hirun.web.biz;
 
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.hirun.pub.domain.entity.org.EmployeeEntity;
+import com.hirun.pub.domain.entity.user.UserEntity;
 import com.most.core.pub.data.ServiceRequest;
 import com.most.core.pub.data.ServiceResponse;
+import com.most.core.pub.tools.datastruct.ArrayTool;
 import com.most.core.web.RootController;
 import com.most.core.web.client.ServiceClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController extends RootController {
+
+    @RequestMapping("/initUser")
+    public @ResponseBody String initUser(HttpServletRequest request, HttpSession session) throws Exception{
+        UserEntity user = (UserEntity)session.getAttribute("USER");
+        EmployeeEntity employee = (EmployeeEntity) session.getAttribute("EMPLOYEE");
+        JSONArray jobRoles = (JSONArray)session.getAttribute("JOB_ROLE");
+
+        logger.debug("==========================jobRoles========================"+jobRoles);
+
+        String path = request.getContextPath();
+        String basePath = request.getScheme()+"://" +request.getServerName()+":" +request.getServerPort()+path+"/" ;
+
+        JSONObject rst = new JSONObject();
+        if(employee != null){
+            rst.put("NAME", employee.getName());
+            String jobRoleName = "";
+            String orgName = "";
+            if(ArrayTool.isNotEmpty(jobRoles)){
+                int size = jobRoles.size();
+                for(int i=0;i<size;i++){
+                    JSONObject jobRole = jobRoles.getJSONObject(i);
+                    String tempJobRoleName = jobRole.getString("JOB_ROLE_NAME");
+                    if(StringUtils.isBlank(jobRoleName)){
+                        jobRoleName += tempJobRoleName;
+                    }
+                    else{
+                        jobRoleName += "/"+tempJobRoleName;
+                    }
+                    JSONObject org = jobRole.getJSONObject("ORG_INFO");
+                    if(org != null){
+                        String tempOrgName = org.getString("NAME");
+                        if(StringUtils.isBlank(orgName)){
+                            orgName += tempOrgName;
+                        }
+                        else{
+                            orgName += "/" + orgName;
+                        }
+                    }
+
+                }
+            }
+            rst.put("ORG_NAME", orgName);
+            rst.put("JOB_ROLE_NAME", jobRoleName);
+
+            if("1".equals(employee.getSex())){
+                rst.put("HEAD_IMAGE", basePath + "frame/img/male.png");
+            }
+            else{
+                rst.put("HEAD_IMAGE", basePath + "frame/img/female.png");
+            }
+        }
+        else{
+            rst.put("NAME", user.getUserName());
+            rst.put("ORG_NAME", "");
+            rst.put("JOB_ROLE_NAME", "");
+            rst.put("HEAD_IMAGE", basePath + "frame/img/male.png");
+        }
+        logger.debug("=================rst==================="+rst.toJSONString());
+        return rst.toJSONString();
+    }
 
     @RequestMapping("/initMenu")
     public @ResponseBody String initMenu(HttpSession session) throws Exception{
         ServiceRequest request = new ServiceRequest();
         ServiceResponse response = ServiceClient.call("OrgCenter.menu.MenuService.loadMenus", request);
         JSONArray menus = response.getJSONArray("MENUS");
-        logger.debug("=============================menus length====================="+menus.size());
         session.setAttribute("MENUS", menus);
         return menus.toJSONString();
     }
