3b3427f48fe1fcef1eee60a4f041c9da8c51b978
jinnian <38103152@qq.com>
2018-05-07 11:01:32 +0800

    startup

--- most-pub/src/main/java/com/most/core/pub/data/SessionEntity.java
+++ most-pub/src/main/java/com/most/core/pub/data/SessionEntity.java
@@ -1,5 +1,6 @@
 package com.most.core.pub.data;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -30,5 +31,37 @@ public class SessionEntity {
 
     public void setUserId(String userId) {
         this.data.put("USER_ID", userId);
+    }
+
+    public String getUsername(){
+        return data.getString("USERNAME");
+    }
+
+    public void setUsername(String username){
+        this.data.put("USERNAME", username);
+    }
+
+    public String get(String key){
+        return this.data.getString(key);
+    }
+
+    public JSONObject getJSONObject(String key){
+        return this.data.getJSONObject(key);
+    }
+
+    public JSONArray getJSONArray(String key){
+        return this.data.getJSONArray(key);
+    }
+
+    public void put(String key, String value){
+        this.data.put(key, value);
+    }
+
+    public void put(String key, JSONObject json){
+        this.data.put(key, json);
+    }
+
+    public void put(String key, JSONArray jsonArray){
+        this.data.put(key, jsonArray);
     }
 }
