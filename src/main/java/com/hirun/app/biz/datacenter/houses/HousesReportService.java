a59653220efbafb38d58695bff089f1920a8d71d
jinniananswer <38103152@qq.com>
2018-06-12 23:26:20 +0800

    jinnian

--- src/main/java/com/hirun/app/biz/datacenter/houses/HousesReportService.java
+++ src/main/java/com/hirun/app/biz/datacenter/houses/HousesReportService.java
@@ -1,5 +1,7 @@
 package com.hirun.app.biz.datacenter.houses;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.hirun.app.dao.houses.HousesPlanDAO;
 import com.hirun.app.dao.org.OrgDAO;
 import com.most.core.app.database.dao.factory.DAOFactory;
@@ -99,6 +101,70 @@ public class HousesReportService extends GenericService {
         ServiceResponse response = new ServiceResponse();
         response.set("COUNSELOR_PLAN", ConvertTool.toJSONArray(plan));
         response.set("COUNSELOR_ACTUAL", ConvertTool.toJSONArray(actual));
+        if(company != null)
+            response.set("COMPANYS", ConvertTool.toJSONArray(company));
+
+        return response;
+    }
+
+    public ServiceResponse reportCounselorLoophole(ServiceRequest request) throws Exception{
+        String orgId = request.getString("ORG_ID");
+        RecordSet plan = null;
+        RecordSet actual = null;
+        HousesPlanDAO dao = DAOFactory.createDAO(HousesPlanDAO.class);
+
+        RecordSet company = new RecordSet();
+        Record record = new Record();
+        record.put("NAME","所有分公司");
+        record.put("ORG_ID","-1");
+        company.add(record);
+        if(orgId == null){
+            OrgDAO orgDAO = DAOFactory.createDAO(OrgDAO.class);
+            RecordSet rst = orgDAO.queryCompany();
+            company.addAll(rst);
+        }
+
+
+        if(orgId == null || StringUtils.equals("-1", orgId)){
+            plan = dao.getPlanCounselorNumByCompany();
+            actual = dao.getActualCounselorNumByCompany();
+        }
+        else{
+            plan = dao.getPlanCounselorNumByShop(orgId);
+            actual = dao.getActualCounselorNumByShop(orgId);
+        }
+
+        ServiceResponse response = new ServiceResponse();
+        JSONArray jsonArray = new JSONArray();
+        int num = 0;
+        if(plan != null && plan.size() > 0){
+            int size = plan.size();
+            for(int i=0;i<size;i++){
+                Record planRecord = plan.get(i);
+                int planNum = planRecord.getInt("PLAN_NUM");
+                String planOrgId = planRecord.get("ORG_ID");
+                int actualNum = 0;
+                if(actual !=null && actual.size() > 0){
+                    int actualSize = actual.size();
+                    for(int j=0;j<actualSize;j++){
+                        Record actualRecord = actual.get(j);
+                        String actualOrgId = actualRecord.get("ORG_ID");
+                        if(StringUtils.equals(planOrgId, actualOrgId)){
+                            actualNum = actualRecord.getInt("ACTUAL_NUM");
+                            break;
+                        }
+                    }
+                }
+                JSONObject json = new JSONObject();
+                json.put("ORG_ID", planOrgId);
+                json.put("NAME", planRecord.get("NAME"));
+                json.put("LOOPHOLE", planNum-actualNum);
+                num = num+(planNum-actualNum);
+                jsonArray.add(json);
+            }
+        }
+        response.set("LOOPHOLE", jsonArray);
+        response.set("NUM", num);
         if(company != null)
             response.set("COMPANYS", ConvertTool.toJSONArray(company));
 
