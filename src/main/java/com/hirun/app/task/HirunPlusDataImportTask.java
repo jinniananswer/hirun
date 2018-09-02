892f92a2f94c74d712266d1268d4924f933688c1
awx0415 <awx0415@163.com>
2018-09-02 13:19:40 +0800

    anwx

--- src/main/java/com/hirun/app/task/HirunPlusDataImportTask.java
+++ src/main/java/com/hirun/app/task/HirunPlusDataImportTask.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.most.core.app.service.invoker.ServiceInvoker;
 import com.most.core.pub.data.ServiceRequest;
 import com.most.core.pub.data.ServiceResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -15,6 +17,9 @@ import java.util.Map;
  * Created by pc on 2018-05-20.
  */
 public class HirunPlusDataImportTask implements Job {
+
+    private static Logger log = LogManager.getLogger(HirunPlusDataImportTask.class);
+
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         Map parameter = new HashMap();
@@ -23,7 +28,7 @@ public class HirunPlusDataImportTask implements Job {
         try {
             ServiceInvoker.invoke("OperationCenter.plan.DataImportTaskService.hirunplusDataImport", request);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("数据导入后台任务执行失败：", e);
         }
     }
 }
