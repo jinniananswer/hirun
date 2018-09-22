921f65e601aa0759b911ac8384d1fbc9529fb0ed
jinniananswer <38103152@qq.com>
2018-09-22 11:06:01 +0800

    jinnian

--- src/main/java/com/hirun/app/task/AutoUpdateHouseNatureTask.java
+++ src/main/java/com/hirun/app/task/AutoUpdateHouseNatureTask.java
@@ -1,12 +1,28 @@
 package com.hirun.app.task;
 
+import com.alibaba.fastjson.JSONObject;
+import com.most.core.app.service.invoker.ServiceInvoker;
+import com.most.core.pub.data.ServiceRequest;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AutoUpdateHouseNatureTask implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-
+        Map parameter = new HashMap();
+        JSONObject jsonObject = new JSONObject(parameter);
+        ServiceRequest request = new ServiceRequest(jsonObject);
+        try {
+            ServiceInvoker.invoke("OperationCenter.house.HousesService.autoUpdateNature", request);
+        } catch (Exception e) {
+            Logger log = LogManager.getLogger(AutoUpdateHouseNatureTask.class.getName());
+            log.error(e);
+        }
     }
 }
