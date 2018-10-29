1a993c5823e99aa96c0c9e7616209896519ed8c6
jinniananswer <38103152@qq.com>
2018-10-29 16:24:29 +0800

    jinnian

--- src/main/webapp/scripts/biz/organization/course/view.courseware.js
+++ src/main/webapp/scripts/biz/organization/course/view.courseware.js
@@ -1,12 +1,7 @@
 (function($){
     $.extend({courseware:{
             init : function() {
-                window["UI-popup"] = new Wade.Popup("UI-popup");
-                $.ajaxPost('initPreworkCourseQuery',null,function(data){
-                    var rst = $.DataMap(data);
-                    $.courseware.drawCourseType(rst.get("COURSE"));
-                    $.courseware.drawCourse(rst.get("COURSE_LIST"));
-                });
+
             }
         }});
 })($);
