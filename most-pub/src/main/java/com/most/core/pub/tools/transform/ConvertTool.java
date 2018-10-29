3ad28b8f3eeeebc6db513eae02e8f4aee31183d0
jinniananswer <38103152@qq.com>
2018-10-29 16:25:28 +0800

    jinnian

--- most-pub/src/main/java/com/most/core/pub/tools/transform/ConvertTool.java
+++ most-pub/src/main/java/com/most/core/pub/tools/transform/ConvertTool.java
@@ -98,6 +98,11 @@ public class ConvertTool {
         return jsonObject;
     }
 
+    public static JSONObject toJSONObject(Record record) {
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(record.getData()));
+        return jsonObject;
+    }
+
     public static void copy(JSONObject source, String[] sourceCols, JSONObject dest, String[] destCols) {
         for(int i = 0, size = sourceCols.length; i <size; i++) {
             dest.put(destCols[i], source.getString(sourceCols[i]));
