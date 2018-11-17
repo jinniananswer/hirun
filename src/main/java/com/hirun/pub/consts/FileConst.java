f4b0e305d2aae6f338ed061421825d164a4b1a39
jinniananswer <38103152@qq.com>
2018-11-17 22:18:37 +0800

    jinnian

---
+++ src/main/java/com/hirun/pub/consts/FileConst.java
@@ -0,0 +1,14 @@
+package com.hirun.pub.consts;
+
+/**
+ * @program: hirun
+ * @description: ${description}
+ * @author: jinnian
+ * @create: 2018-11-12 21:07
+ **/
+public class FileConst {
+
+    public static final String OFFICE_FILE_ANALYSIS_PREFIX = "http%3A%2F%2Fwww.hi-run.net%3A8080%2Fdoc%2F";
+
+    public static final String SUPPORT_FILE_TYPE = "|.doc|.docx|.xls|.xlsx|.ppt|.pptx|";
+}
