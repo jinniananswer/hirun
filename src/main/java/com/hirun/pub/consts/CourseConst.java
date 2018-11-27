354e3958d2846fa0e9db9ee0afa53b4a383f4676
jinniananswer <38103152@qq.com>
2018-11-28 00:22:45 +0800

    jinnian

---
+++ src/main/java/com/hirun/pub/consts/CourseConst.java
@@ -0,0 +1,12 @@
+package com.hirun.pub.consts;
+
+/**
+ * @program: hirun
+ * @description: ${description}
+ * @author: jinnian
+ * @create: 2018-11-27 01:08
+ **/
+public class CourseConst {
+
+    public static final String COURSE_ROOT = "鸿扬课程体系";
+}
--- src/main/java/com/hirun/pub/consts/FileConst.java
+++ src/main/java/com/hirun/pub/consts/FileConst.java
@@ -11,4 +11,6 @@ public class FileConst {
     public static final String OFFICE_FILE_ANALYSIS_PREFIX = "http%3A%2F%2Fwww.hi-run.net%3A8080%2Fdoc%2F";
 
     public static final String SUPPORT_FILE_TYPE = "|.doc|.docx|.xls|.xlsx|.ppt|.pptx|";
+
+    public static final String STORAGE_PATH = "http://139.129.29.141:8080/doc/";
 }
