97efdcc572ba60154a67a3128847c59ab0994f62
jinniananswer <38103152@qq.com>
2019-01-12 20:09:32 +0800

    jinnian

--- most-pub/src/main/java/com/most/core/pub/tools/Utility.java
+++ most-pub/src/main/java/com/most/core/pub/tools/Utility.java
@@ -3,6 +3,8 @@ package com.most.core.pub.tools;
 import com.alibaba.fastjson.JSONObject;
 import com.most.core.pub.exception.GenericException;
 
+import java.util.Random;
+
 /**
  * Created by awx on 2018/7/22/022.
  */
@@ -28,5 +30,32 @@ public class Utility {
         if(errorMsg.length() > 0) {
             throw new GenericException("-1", "【" + errorMsg.substring(0, errorMsg.length() - 1) + "】不能为空");
         }
+    }
+
+    public static int[] randomArray(int min,int max,int n){
+        int len = max-min+1;
+
+        if(max < min || n > len){
+            return null;
+        }
+
+        //初始化给定范围的待选数组
+        int[] source = new int[len];
+        for (int i = min; i < min+len; i++){
+            source[i-min] = i;
+        }
+
+        int[] result = new int[n];
+        Random rd = new Random();
+        int index = 0;
+        for (int i = 0; i < result.length; i++) {
+            //待选数组0到(len-2)随机一个下标
+            index = Math.abs(rd.nextInt() % len--);
+            //将随机到的数放入结果集
+            result[i] = source[index];
+            //将待选数组中被随机到的数，用待选数组(len-1)下标对应的数替换
+            source[index] = source[len];
+        }
+        return result;
     }
 }
