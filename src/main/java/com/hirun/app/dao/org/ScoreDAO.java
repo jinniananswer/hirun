5e15f82ce1f445a64bf2c9cdc799bdef44374c78
liuhui <lh936679052@163.com>
2019-02-16 22:54:57 +0800

    liuhui

--- src/main/java/com/hirun/app/dao/org/ScoreDAO.java
+++ src/main/java/com/hirun/app/dao/org/ScoreDAO.java
@@ -5,7 +5,6 @@ import com.most.core.app.database.dao.GenericDAO;
 import com.most.core.pub.data.RecordSet;
 import org.apache.commons.lang3.StringUtils;
 
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,7 +46,7 @@ public class ScoreDAO extends GenericDAO {
     }
 
 
-    public RecordSet queryPostJobScore(String name,String orgId) throws Exception{
+    public RecordSet queryPostJobScore(String name,String orgId,String train_id) throws Exception{
         Map<String, String> parameter = new HashMap<String, String>();
 
         StringBuilder sb = new StringBuilder();
@@ -55,7 +54,8 @@ public class ScoreDAO extends GenericDAO {
         sb.append("select a.USER_ID,b.NAME,b.employee_id,b.sex,a.mobile_no contact_no, d.JOB_ROLE, e.name org_name, e.city , f.NAME parent_org_name ,g.TRAIN_ID,g.score ");
         sb.append("from ins_user a, ins_employee b ");
         sb.append("left join ins_train_exam_score g on(b.EMPLOYEE_ID=g.EMPLOYEE_ID) , ");
-        sb.append("ins_employee_job_role d, ins_org e ");
+        sb.append("ins_train_sign h , ");
+        sb.append("ins_employee_job_role d, ins_org e  ");
         sb.append("left join ins_org f on(f.ORG_ID = e.PARENT_ORG_ID) ");
         sb.append("where b.USER_ID = a.USER_ID ");
         sb.append("and d.EMPLOYEE_ID = b.EMPLOYEE_ID ");
@@ -63,7 +63,7 @@ public class ScoreDAO extends GenericDAO {
         sb.append("and b.status = '0' " );
         sb.append("and now() < d.end_date ");
         sb.append("and e.ORG_ID = d.ORG_ID ");
-        sb.append("and b.EMPLOYEE_ID = g.EMPLOYEE_ID ");
+        sb.append("and h.EMPLOYEE_ID=b.EMPLOYEE_ID ");
 
         if(StringUtils.isNotBlank(name)) {
             sb.append("and b.name like concat('%',:NAME,'%') ");
@@ -72,6 +72,12 @@ public class ScoreDAO extends GenericDAO {
 
         if(StringUtils.isNotBlank(orgId)){
             sb.append("and d.org_id in ( "+orgId+") ");
+        }
+
+        if(StringUtils.isNotBlank(train_id)){
+            sb.append("and h.train_id = "+train_id+" ");
+            sb.append("and h.STATUS ='0' ");
+
         }
 
         return this.queryBySql(sb.toString(),parameter);
