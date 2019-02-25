02da75282ec497f1dce17628021d57f85b4e214b
jinniananswer <38103152@qq.com>
2019-02-25 22:43:38 +0800

    jinnian

--- src/main/webapp/scripts/biz/organization/train/sign.list.js
+++ src/main/webapp/scripts/biz/organization/train/sign.list.js
@@ -2,8 +2,28 @@
     $.extend({train:{
             index : 0,
             currentIndex : 0,
+            addEmployees : ",",
+            existsEmployees : ",",
+            addLength : 0,
             init : function() {
+                window["UI-popup"] = new Wade.Popup("UI-popup");
+                window["orgTree"] = new Wade.Tree("orgTree");
+                $("#orgTree").textAction(function(e, nodeData){
+                    var id = nodeData.id;
+                    var text = nodeData.text;
+                    $("#ORG_TEXT").val(text);
+                    $("#ORG_ID").val(id);
+
+                    backPopup(document.getElementById('UI-ORG'));
+                    return true;
+                });
+
                 $.ajaxPost('initQuerySignList','&TRAIN_ID='+$("#TRAIN_ID").val(),function(data) {
+                    var trees = data.ORG_TREE;
+                    if(trees != null){
+                        window["orgTree"].data = trees;
+                        window["orgTree"].init();
+                    }
                     var rst = new Wade.DataMap(data);
                     var signs = rst.get("SIGN_LIST");
                     var train = rst.get("TRAIN");
@@ -11,10 +31,19 @@
                     if(signStatus == "0") {
                         $("#DELETE_BUTTON").css("display", "");
                         $("#END_SIGN_BUTTON").css("display", "");
+                        $("#ADD_BUTTON").css("display", "none");
                     }
                     else {
-                        $("#DELETE_BUTTON").css("display", "none");
                         $("#END_SIGN_BUTTON").css("display", "none");
+                        var hasEndSignOper = rst.get("HAS_END_SIGN_OPER");
+                        if(hasEndSignOper == "true") {
+                            $("#ADD_BUTTON").css("display", "");
+                            $("#DELETE_BUTTON").css("display", "");
+                        }
+                        else {
+                            $("#DELETE_BUTTON").css("display", "none");
+                            $("#DELETE_BUTTON").css("display", "none");
+                        }
                     }
 
                     var total = $("#total_num");
@@ -49,6 +78,7 @@
                     html.push("<ul>");
                     for(var i=0;i<length;i++) {
                         var data = signs.get(i);
+                        $.train.existsEmployees += data.get("EMPLOYEE_ID") + ",";
                         html.push("<li class='link'><div class=\"group\"><div class=\"content\"><div class='l_padding'><div class=\"pic pic-middle\">");
                         html.push("</div></div>");
                         html.push("<div class=\"main\"><div class=\"title\">");
@@ -185,6 +215,130 @@
             endSign : function() {
                 $.ajaxPost('endSign', "&TRAIN_ID="+$("#TRAIN_ID").val(), function (data) {
                     MessageBox.success("终止报名成功","点击确定返回当前页面，点击取消关闭当前页面", function(btn){
+                        if("ok" == btn) {
+                            document.location.reload();
+                        }
+                        else {
+                            $.redirect.closeCurrentPage();
+                        }
+                    },{"cancel":"取消"})
+                });
+            },
+
+            drawJobRoles : function(){
+                if(this.jobs == null || this.jobs == "undefined")
+                    return;
+
+                var length = this.jobs.length;
+                var html = [];
+                $("#jobRoles").empty();
+                for(var i=0;i<length;i++){
+                    var jobRole = this.jobs.get(i);
+                    html.push("<li class=\"link e_center\" ontap=\"$.train.afterSelectJob(\'"+jobRole.get("CODE_VALUE")+"\',\'"+jobRole.get("CODE_NAME")+"\')\"><label class=\"group\" ><div class=\"main\">"+jobRole.get("CODE_NAME")+"</div></label></li>");
+                }
+                $.insertHtml('beforeend', $("#jobRoles"), html.join(""));
+            },
+
+            afterSelectJob : function(value, name){
+                $("#JOB_TEXT").val(name);
+                $("#JOB_ROLE").val(value);
+                backPopup(document.getElementById('UI-JOB'));
+            },
+
+            queryJobs : function(){
+                var searchText = $("#JOB_SEARCH_TEXT").val();
+                $("#jobRoles").empty();
+                var length = this.jobs.length;
+                var html = [];
+                for(var i=0;i<length;i++){
+                    var jobRole = this.jobs.get(i);
+                    var name = jobRole.get("CODE_NAME");
+                    if(name.indexOf(searchText) >= 0 || searchText == "")
+                        html.push("<li class=\"link e_center\" ontap=\"$.train.afterSelectJob(\'"+jobRole.get("CODE_VALUE")+"\',\'"+jobRole.get("CODE_NAME")+"\')\"><label class=\"group\" ><div class=\"main\">"+jobRole.get("CODE_NAME")+"</div></label></li>");
+                }
+                $.insertHtml('beforeend', $("#jobRoles"), html.join(""));
+            },
+
+            query : function() {
+                $.beginPageLoading();
+                var parameter = $.buildJsonData("queryArea");
+                $.ajaxPost('queryWantSignEmployee',parameter,function(data) {
+                    var rst = new Wade.DataMap(data);
+                    var employees = rst.get("EMPLOYEE");
+                    $.train.drawSelectEmployees(employees);
+                });
+            },
+
+            drawSelectEmployees : function(datas) {
+                if(datas == null || datas.length <= 0) {
+                    $("#messagebox_employee").css("display", "");
+                    return;
+                }
+                $("#select_employees").empty();
+                $("#messagebox_employee").css("display", "none");
+                var html = [];
+
+                var length = datas.length;
+                for(var i=0;i<length;i++) {
+                    var data = datas.get(i);
+                    html.push("<li class='link' employeeId='"+data.get("EMPLOYEE_ID")+"' sex='"+data.get("SEX")+"' employeeName='"+data.get("NAME")+"' ontap=\"$.train.selectEmployee(this)\"><div class=\"group\"><div class=\"content\">");
+                    html.push("<div class=\"main\"><div class=\"title\">");
+                    html.push(data.get("NAME"));
+                    html.push("</div>");
+                    html.push("<div class=\"content\">");
+                    var allOrgName = data.get("ALL_ORG_NAME");
+                    html.push(allOrgName);
+                    html.push("</div><div class='content'>"+data.get("JOB_ROLE_NAME"));
+                    html.push("</div></div>")
+                    html.push("</div></div></li>");
+                }
+
+                $.insertHtml('beforeend', $("#select_employees"), html.join(""));
+                $.endPageLoading();
+            },
+
+            selectEmployee : function(obj) {
+                var li = $(obj);
+                var className = li.attr("class");
+                if(className == "link checked") {
+                    li.attr("class", "link");
+                }
+                else {
+                    li.attr("class", "link checked");
+                }
+            },
+
+            confirmSelectEmployee : function() {
+                var lis = $("#select_employees li");
+
+                if(lis == null || lis.length <= 0) {
+                    MessageBox.alert("您没有选中任何员工，请先选择员工");
+                    return;
+                }
+
+                var length = lis.length;
+                for(var i=0;i<length;i++) {
+                    var li = $(lis[i]);
+                    var className = li.attr("class");
+                    if(className == "link checked") {
+                        if(this.existsEmployees.indexOf(","+li.attr("employeeId")+",") >= 0) {
+                            continue;
+                        }
+                        this.addEmployees += li.attr("employeeId")+",";
+                        this.addLength++;
+                    }
+                }
+                if(this.addLength <= 0) {
+                    MessageBox.alert("您没有选中任何要增加的员工，或者所选的员工已经报过名了，请重新选择");
+                    return;
+                }
+                var employeeIds = this.addEmployees.substring(1,this.addEmployees.length - 1);
+                var parameter = '&NEW_EMPLOYEE_ID='+employeeIds+"&TRAIN_ID="+$("#TRAIN_ID").val();
+                $.beginPageLoading();
+                $.ajaxPost('signNewEmployee', parameter, function (data) {
+                    $.endPageLoading();
+
+                    MessageBox.success("新增报名人员成功","点击确定返回新增页面，点击取消关闭当前页面", function(btn){
                         if("ok" == btn) {
                             document.location.reload();
                         }
