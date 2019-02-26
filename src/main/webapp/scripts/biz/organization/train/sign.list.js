2b58ed05197a699a6e945319516e0f0919001d77
jinniananswer <38103152@qq.com>
2019-02-26 23:17:12 +0800

    jinnian

--- src/main/webapp/scripts/biz/organization/train/sign.list.js
+++ src/main/webapp/scripts/biz/organization/train/sign.list.js
@@ -271,6 +271,7 @@
 
             drawSelectEmployees : function(datas) {
                 if(datas == null || datas.length <= 0) {
+                    $.endPageLoading();
                     $("#messagebox_employee").css("display", "");
                     return;
                 }
