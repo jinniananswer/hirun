8c8c8b8cc0e546b781c2263fa2673e4b8cdbbf92
jinniananswer <38103152@qq.com>
2019-03-15 22:52:05 +0800

    jinnian

--- src/main/java/com/hirun/web/biz/organization/train/TrainController.java
+++ src/main/java/com/hirun/web/biz/organization/train/TrainController.java
@@ -1,15 +1,23 @@
 package com.hirun.web.biz.organization.train;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.most.core.pub.data.ServiceResponse;
+import com.most.core.pub.tools.datastruct.ArrayTool;
+import com.most.core.pub.tools.office.ExcelExport;
 import com.most.core.web.RootController;
 import com.most.core.web.client.ServiceClient;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+import java.util.*;
 
 /**
  * @program: hirun
@@ -161,5 +169,91 @@ public class TrainController extends RootController {
     @RequestMapping("/redirectToTrainScoreQuery")
     public String redirectToTrainScoreQuery(HttpServletRequest request) throws Exception {
         return "/biz/organization/score/score_query";
+    }
+
+    @RequestMapping("/exportSignList")
+    public @ResponseBody void exportSignList(HttpServletRequest request, HttpServletResponse httpResponse) throws Exception{
+        Map<String, String> parameter = new HashMap<String, String>();
+        parameter.put("TRAIN_ID", request.getParameter("TRAIN_ID"));
+        ServiceResponse response = ServiceClient.call("OrgCenter.train.TrainService.initQuerySignList", parameter);
+
+        JSONObject train = response.getJSONObject("TRAIN");
+        JSONObject signs = response.getJSONObject("SIGN_LIST");
+        String sheetName = train.getString("NAME");
+
+        if(signs == null || signs.size() <= 0){
+            return;
+        }
+
+        List<String> titles = new ArrayList<String>();
+        titles.add("姓名");
+        titles.add("归属公司");
+        titles.add("归属部门");
+        titles.add("性别");
+        titles.add("工作岗位");
+        titles.add("联系电话");
+        titles.add("毕业院校");
+        titles.add("学历");
+        titles.add("专业");
+        titles.add("毕业证书编号");
+        titles.add("入职日期");
+
+        Set<String> companys = signs.keySet();
+        List<List<String>> values = new ArrayList<List<String>>();
+        for(String companyName : companys) {
+            JSONArray datas = signs.getJSONArray(companyName);
+
+            if(ArrayTool.isEmpty(datas)) {
+                continue;
+            }
+            int size = datas.size();
+            for(int i=0;i<size;i++) {
+                JSONObject data = datas.getJSONObject(i);
+                List<String> value = new ArrayList<String>();
+                value.add(data.getString("NAME"));
+                value.add(data.getString("ENTERPRISE_NAME"));
+                value.add(data.getString("ALL_ORG_NAME"));
+                String sex = data.getString("SEX");
+                String sexName = "";
+                if(StringUtils.equals("1", sex)) {
+                    sexName = "男";
+                }
+                else {
+                    sexName = "女";
+                }
+                value.add(sexName);
+                value.add(data.getString("JOB_ROLE_NAME"));
+                value.add(data.getString("MOBILE_NO"));
+                value.add(data.getString("SCHOOL"));
+                value.add(data.getString("EDUCATION"));
+                value.add(data.getString("MAJOR"));
+                value.add(data.getString("CERTIFICATE_NO"));
+                value.add(data.getString("IN_DATE"));
+                values.add(value);
+            }
+        }
+
+        HSSFWorkbook excel = ExcelExport.getDataExcel(sheetName, titles, values);
+
+        String fileName = "培训报名表-"+sheetName+".xls";
+        fileName = new String(fileName.getBytes(),"ISO8859-1");
+
+        httpResponse.setContentType("application/octet-stream;charset=ISO8859-1");
+        httpResponse.setHeader("Content-Disposition", "attachment;filename="+ fileName);
+        httpResponse.addHeader("Pargam", "no-cache");
+        httpResponse.addHeader("Cache-Control", "no-cache");
+
+        OutputStream os = httpResponse.getOutputStream();
+        try {
+            excel.write(os);
+        }
+        catch (Exception e) {
+
+        }
+        finally {
+            os.flush();
+            os.close();
+        }
+
     }
 }
