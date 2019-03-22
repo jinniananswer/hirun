cd4fd3c91fd4529e891537b67efe9f13e91cb5d4
jinniananswer <38103152@qq.com>
2019-03-23 01:39:57 +0800

    jinnian

--- src/main/java/com/hirun/app/dao/org/TrainDAO.java
+++ src/main/java/com/hirun/app/dao/org/TrainDAO.java
@@ -155,7 +155,7 @@ public class TrainDAO extends GenericDAO {
         parameter.put("TRAIN_ID", trainId);
 
         StringBuilder sql = new StringBuilder();
-        sql.append("select a.train_id, a.employee_id,a.status, b.name, b.identity_no, b.sex,b.major, b.school, b.education_level,b.certificate_no, date_format(b.in_date, '%Y-%m-%d') in_date, c.org_id, c.job_role, d.name org_name, e.name enterprise_name, f.type, f.item, g.mobile_no, v.view_id ");
+        sql.append("select a.train_id, a.employee_id,a.status, b.name, b.identity_no, b.sex,b.major, b.school, b.education_level,b.certificate_no, date_format(b.in_date, '%Y-%m-%d') in_date, c.org_id, c.job_role, d.name org_name, e.name enterprise_name, f.type, f.item,f.in_canteen, g.mobile_no, v.view_id ");
         sql.append("from ins_train_sign a left join ins_train_notice_view v on (v.train_id = a.train_id and v.employee_id = a.employee_id), ins_employee b, ins_employee_job_role c, ins_org d, ins_enterprise e, ins_train_sign_item f, ins_user g ");
         sql.append("where b.employee_id = a.employee_id ");
         sql.append("and c.employee_id = a.employee_id ");
