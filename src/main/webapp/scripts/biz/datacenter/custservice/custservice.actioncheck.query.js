721b9daae210a8927859b61ab182a073ddb9efe6
liuhui <lh936679052@163.com>
2019-06-30 15:41:55 +0800

    liuhui

---
+++ src/main/webapp/scripts/biz/datacenter/custservice/custservice.actioncheck.query.js
@@ -0,0 +1,238 @@
+(function($){
+    $.extend({custServiceActionQuery:{
+            init : function() {
+
+                window["UI-popup"] = new Wade.Popup("UI-popup",{
+                    visible:false,
+                    mask:true
+                });
+
+                window["COND_START_DATE"] = new Wade.DateField(
+                    "COND_START_DATE",
+                    {
+                        dropDown:true,
+                        format:"yyyy-MM-dd",
+                        useTime:false,
+                    }
+                );
+
+                window["COND_END_DATE"] = new Wade.DateField(
+                    "COND_END_DATE",
+                    {
+                        dropDown:true,
+                        format:"yyyy-MM-dd",
+                        useTime:false,
+                    }
+                );
+
+                window["orgTree"] = new Wade.Tree("orgTree");
+
+                $("#orgTree").textAction(function(e, nodeData){
+                    var id = nodeData.id;
+                    var text = nodeData.text;
+                    $("#ORG_TEXT").val(text);
+                    $("#ORG_ID").val(id);
+
+                    backPopup(document.getElementById('UI-ORG'));
+                    return true;
+                });
+
+
+
+                var now = $.date.now();
+                $('#COND_START_DATE').val(now);
+                $('#COND_END_DATE').val(now);
+
+
+                $.ajaxPost('initQueryActionFinishInfo',null,function(data){
+                    var trees = data.ORG_TREE;
+                   // var rst = new Wade.DataMap(data);
+                   // var datas = rst.get("CUSTSERVICEINFO");
+
+                    if(trees != null){
+                        window["orgTree"].data = trees;
+                        window["orgTree"].init();
+
+                    }
+
+                   // $.custServiceActionQuery.drawCustServiceInfo4Query(datas);
+                    hidePopup('UI-popup','QueryCondPopupItem');
+                    hidePopup('UI-popup','UI-QUERYCUSTSERVICE');
+                    hidePopup('UI-popup','UI-ORG');
+
+
+                });
+
+            },
+
+
+
+            query :function(){
+                $.beginPageLoading("查询中。。。");
+
+                var custServiceEmpId=$("#CUSTSERVICEEMPLOYEEID").val();
+                var orgId=$("#ORG_ID").val();
+                var startDate=$('#COND_START_DATE').val();
+                var endDate=$('#COND_END_DATE').val();
+
+                var param='&CUSTSERVICEEMPID='+custServiceEmpId+"&ORG_ID="+orgId+"&START_DATE="+startDate+"&END_DATE="+endDate;
+                $.ajaxPost('queryCustServFinishActionInfo',param,function(data) {
+                    var rst = new Wade.DataMap(data);
+                    var datas=rst.get("CUSTSERVICEFINISHACTIONINFO");
+                    $.custServiceActionQuery.drawActionInfo(datas);
+                    hidePopup('UI-popup','QueryCondPopupItem');
+
+                });
+            },
+
+            queryCustService :function(){
+                $.beginPageLoading("查询中。。。");
+
+                var custServiceName=$("#CUSTSERVICE_NAME").val();
+
+                var param='&CUSTSERVICE_NAME='+custServiceName;
+                $.ajaxPost('queryCustServiceByName',param,function(data) {
+                    var rst = new Wade.DataMap(data);
+                    var datas=rst.get("CUSTSERVICEINFO");
+                    $.custServiceActionQuery.drawCustServiceInfo4Query(datas);
+
+                });
+            },
+
+            drawActionInfo : function(datas){
+                $.endPageLoading();
+                $("#actioninfo").empty();
+                var html = [];
+
+                if(datas == null || datas.length <= 0){
+                    $("#queryMessage").css("display","");
+                    return;
+                }
+                $("#queryMessage").css("display","none");
+
+
+
+                var length = datas.length;
+                for(var i=0;i<length;i++) {
+                    var data = datas.get(i);
+                    var partyName=data.get("PARTY_NAME");
+                    var create_date=data.get("CREATE_DATE");
+                    var houseAddress=data.get("HOUSE_ADDRESS");
+                    var custservName=data.get("CUSTSERVICENAME");
+                    var scanCityTime=data.get("EXPERIENCE_TIME");
+                    var scanCityCabins=data.get("CITYCABINNAMES");
+                    var experience=data.get("EXPERIENCE");
+                    var finishTime=data.get("FINISH_TIME");
+                    var funcPrintTime=data.get("FUNCPRINTTIME");
+                    var stylePrintTime=data.get("STYLEPRINTTIME");
+                    var wxnick=data.get("WX_NICK");
+
+                    html.push("<tr>");
+                    html.push("<th class='red'>");
+                    html.push(create_date.substr(0,19));
+                    html.push("</th><th class='red'>")
+                    html.push(partyName);
+                    html.push("</th>");
+
+                    html.push("<th class='red'>");
+                    if(wxnick=="undefined" || wxnick ==null || wxnick =="null"){
+                        html.push("");
+                    }else{
+                        html.push(wxnick)
+                    }
+                    html.push("</th>");
+
+                    html.push("<th class='red'>");
+                    if(houseAddress=="undefined" || houseAddress ==null || houseAddress =="null"){
+                        html.push("");
+                    }else{
+                        html.push(houseAddress)
+                    }
+                    html.push("</th>");
+
+                    html.push("<th class='red'>");
+                    html.push(custservName)
+                    html.push("</th><th class='red'>");
+                    if(finishTime=="undefined" || finishTime ==null){
+                        html.push("");
+                    }else{
+                        html.push(finishTime.substr(0,19))
+                    }
+                    html.push("</th><th class='red'>");
+                    if(stylePrintTime=="undefined" || stylePrintTime ==null){
+                        html.push("");
+                    }else{
+                        html.push(stylePrintTime.substr(0,19))
+                    }
+                    html.push("</th><th class='red'>");
+
+                    if(funcPrintTime=="undefined" || funcPrintTime ==null){
+                        html.push("");
+                    }else{
+                        html.push(funcPrintTime.substr(0,19))
+                    }
+
+                    html.push("</th><th class='red'>");
+                    if(scanCityTime!="undefined" && scanCityTime !=null){
+                        html.push(scanCityTime.substr(0,10));
+                    }else{
+                        html.push(scanCityTime);
+                    }
+                    html.push("</th><th class='red'>");
+                    html.push(scanCityCabins);
+                    html.push("</th><th class='red'>");
+                    html.push(experience)
+                    html.push("</th>")
+                    html.push("</tr>")
+                }
+
+                $.insertHtml('beforeend', $("#actioninfo"), html.join(""));
+            },
+
+            drawCustServiceInfo4Query : function(datas){
+                $.endPageLoading();
+                $("#querycustservicesinfo").empty();
+                var html = [];
+
+                if(datas == null || datas.length <= 0){
+                    //$("#messagebox").css("display","");
+                    $("#submitButton").css("display","none");
+                    return;
+                }
+
+                //$("#messagebox").css("display","none");
+                $("#submitButton").css("display","");
+
+
+                var length = datas.length;
+                for(var i=0;i<length;i++) {
+                    var data = datas.get(i);
+                    html.push("<li class='link' custserviceEmpId='" + data.get("EMPLOYEE_ID") + "' custserviceEmpName='" + data.get("NAME") + "' ontap='$.custServiceActionQuery.selectCustService(this);'><div class=\"group\"><div class=\"content\"><div class='l_padding'><div class=\"pic pic-middle\">");
+                    html.push("</div></div>");
+                    html.push("<div class=\"main\"><div class=\"title\">");
+                    html.push(data.get("NAME"));
+                    html.push("</div>");
+                    html.push("<div class=\"content\">");
+                    html.push("</div>");
+                    html.push("</div>")
+                    html.push("</div></div></li>");
+                }
+
+                $.insertHtml('beforeend', $("#querycustservicesinfo"), html.join(""));
+            },
+
+            selectCustService : function (e) {
+                var obj = $(e);
+                var custserviceEmpId=obj.attr("custserviceEmpId");
+                var custserviceEmpName=obj.attr("custserviceEmpName");
+                $("#CUSTSERVICEEMPLOYEENAME").val(custserviceEmpName);
+                $("#CUSTSERVICEEMPLOYEEID").val(custserviceEmpId);
+                backPopup(document.getElementById("QueryCondPopupItem"));
+
+            },
+
+
+
+
+        }});
+})($);
---
+++ src/main/webapp/scripts/biz/datacenter/custservice/custservice.monstat.query.js
@@ -0,0 +1,207 @@
+(function($){
+    $.extend({custServiceMonstatQuery:{
+            init : function() {
+
+                window["UI-popup"] = new Wade.Popup("UI-popup",{
+                    visible:false,
+                    mask:true
+                });
+
+                var now = $.date.now();
+                var nowYYYYMM = $.date.now().substring(0,4) + $.date.now().substring(5,7);
+
+                window["MON_DATE"] = new Wade.DateField(
+                    "MON_DATE",
+                    {
+                        value:nowYYYYMM,
+                        dropDown:true,
+                        format:"yyyyMM",
+                        useTime:false,
+                        useMode:'month',
+                    }
+                );
+
+
+
+                window["orgTree"] = new Wade.Tree("orgTree");
+                $('#MON_DATE').val(nowYYYYMM);
+
+                $("#orgTree").textAction(function(e, nodeData){
+                    var id = nodeData.id;
+                    var text = nodeData.text;
+                    $("#ORG_TEXT").val(text);
+                    $("#ORG_ID").val(id);
+
+                    backPopup(document.getElementById('UI-ORG'));
+                    return true;
+                });
+
+
+
+                var now = $.date.now();
+                $('#COND_START_DATE').val(now);
+                $('#COND_END_DATE').val(now);
+
+
+                $.ajaxPost('initQueryActionFinishInfo',null,function(data){
+                    var trees = data.ORG_TREE;
+                    var rst = new Wade.DataMap(data);
+                    //var datas = rst.get("CUSTSERVICEINFO");
+
+                    if(trees != null){
+                        window["orgTree"].data = trees;
+                        window["orgTree"].init();
+
+                    }
+
+                    //$.custServiceActionQuery.drawCustServiceInfo4Query(datas);
+                    hidePopup('UI-popup','QueryCondPopupItem');
+                    hidePopup('UI-popup','UI-QUERYCUSTSERVICE');
+                    hidePopup('UI-popup','UI-ORG');
+
+
+                });
+
+            },
+
+
+
+            query :function(){
+                $.beginPageLoading("查询中。。。");
+
+                var custServiceEmpId=$("#CUSTSERVICEEMPLOYEEID").val();
+                var orgId=$("#ORG_ID").val();
+                var monDate=$('#MON_DATE').val();
+
+                var param='&CUSTSERVICEEMPID='+custServiceEmpId+"&ORG_ID="+orgId+"&MON_DATE="+monDate;
+                $.ajaxPost('queryCustServMonStatInfo',param,function(data) {
+                    var rst = new Wade.DataMap(data);
+                    var datas=rst.get("CUSTSERVICESTATINFO");
+                    $.custServiceMonstatQuery.drawStatInfo(datas);
+                    hidePopup('UI-popup','QueryCondPopupItem');
+
+                });
+            },
+
+
+
+
+            queryCustService :function(){
+                $.beginPageLoading("查询中。。。");
+
+                var custServiceName=$("#CUSTSERVICE_NAME").val();
+
+                var param='&CUSTSERVICE_NAME='+custServiceName;
+                $.ajaxPost('queryCustServiceByName',param,function(data) {
+                    var rst = new Wade.DataMap(data);
+                    var datas=rst.get("CUSTSERVICEINFO");
+                    $.custServiceMonstatQuery.drawCustServiceInfo4Query(datas);
+
+                });
+            },
+
+            drawStatInfo : function(datas){
+                $.endPageLoading();
+                $("#statInfo").empty();
+                var html = [];
+
+                if(datas == null || datas.length <= 0){
+                    $("#queryMessage").css("display","");
+                    return;
+                }
+                $("#queryMessage").css("display","none");
+
+
+
+                var length = datas.length;
+                for(var i=0;i<length;i++) {
+                    var data = datas.get(i);
+                    var employeename=data.get("EMPLOYEE_NAME");
+                    var consultcount=data.get("CONSULT_COUNT");
+                    var stylecount=data.get("STYLE_COUNT");
+                    var stylescale=data.get("STYLE_SCALE");
+                    var funccount=data.get("FUNC_COUNT");
+                    var funcscale=data.get("FUNC_SCALE");
+                    var xqltescale=data.get("XQLTE_SCALE");
+                    var scancount=data.get("SCAN_COUNT");
+                    var scanscale=data.get("SCAN_SCALE");
+                    var scancityhousecount=data.get("SCANCITYHOUSE_COUNT");
+                    var scancityhousescale=data.get("SCANCITYHOUSE_SCALE");
+
+                    html.push("<tr>");
+                    html.push("<th class='red'>");
+                    html.push(employeename);
+                    html.push("</th><th class='red'>")
+                    html.push(consultcount);
+                    html.push("</th><th class='red'>");
+                    html.push(stylecount)
+                    html.push("</th><th class='red'>");
+                    html.push(stylescale)
+                    html.push("</th><th class='red'>");
+                    html.push(funccount)
+                    html.push("</th><th class='red'>");
+                    html.push(funcscale)
+                    html.push("</th><th class='red'>");
+                    html.push(xqltescale)
+                    html.push("</th><th class='red'>");
+                    html.push(scancount);
+                    html.push("</th><th class='red'>");
+                    html.push(scanscale);
+                    html.push("</th><th class='red'>");
+                    html.push(scancityhousecount)
+                    html.push("</th><th class='red'>")
+                    html.push(scancityhousescale)
+                    html.push("</th>")
+                    html.push("</tr>")
+                }
+
+                $.insertHtml('beforeend', $("#statInfo"), html.join(""));
+            },
+
+            drawCustServiceInfo4Query : function(datas){
+                $.endPageLoading();
+                $("#querycustservicesinfo").empty();
+                var html = [];
+
+                if(datas == null || datas.length <= 0){
+                    //$("#messagebox").css("display","");
+                    $("#submitButton").css("display","none");
+                    return;
+                }
+
+                //$("#messagebox").css("display","none");
+                $("#submitButton").css("display","");
+
+
+                var length = datas.length;
+                for(var i=0;i<length;i++) {
+                    var data = datas.get(i);
+                    html.push("<li class='link' custserviceEmpId='" + data.get("EMPLOYEE_ID") + "' custserviceEmpName='" + data.get("NAME") + "' ontap='$.custServiceMonstatQuery.selectCustService(this);'><div class=\"group\"><div class=\"content\"><div class='l_padding'><div class=\"pic pic-middle\">");
+                    html.push("</div></div>");
+                    html.push("<div class=\"main\"><div class=\"title\">");
+                    html.push(data.get("NAME"));
+                    html.push("</div>");
+                    html.push("<div class=\"content\">");
+                    html.push("</div>");
+                    html.push("</div>")
+                    html.push("</div></div></li>");
+                }
+
+                $.insertHtml('beforeend', $("#querycustservicesinfo"), html.join(""));
+            },
+
+            selectCustService : function (e) {
+                var obj = $(e);
+                var custserviceEmpId=obj.attr("custserviceEmpId");
+                var custserviceEmpName=obj.attr("custserviceEmpName");
+                $("#CUSTSERVICEEMPLOYEENAME").val(custserviceEmpName);
+                $("#CUSTSERVICEEMPLOYEEID").val(custserviceEmpId);
+                backPopup(document.getElementById("QueryCondPopupItem"));
+
+            },
+
+
+
+
+        }});
+})($);
