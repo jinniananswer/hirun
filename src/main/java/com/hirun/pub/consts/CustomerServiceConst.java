3f3784891456b7a160c8573eb3b421147f282bb2
liuhui <lh936679052@163.com>
2019-06-30 15:33:51 +0800

    liuhui

---
+++ src/main/java/com/hirun/pub/consts/CustomerServiceConst.java
@@ -0,0 +1,16 @@
+package com.hirun.pub.consts;
+
+
+public class CustomerServiceConst {
+
+    public static final String  CUSTOMERSERVICEROLETYPE= "CUSTOMERSERVICE";//客户代表联系角色
+    public static final String  PROJECTDESIGNERROLETYPE= "PROJECTDESIGNER";//项目设计师角色
+
+    public static final String  APPLY_STATUS_0= "0";//客户清理申请状态
+    public static final String  APPLY_STATUS_1= "1";//客户清理审核通过
+    public static final String  APPLY_STATUS_2= "2";//客户清理审核不通过
+
+    public static final String  PARTY_STATUS_0= "0";//客户状态正常
+    public static final String  PARTY_STATUS_1= "1";//客户状态清理
+
+}
