da9845a0e6ae3f2d314ccf8d76f983581a901921
liuhui <lh936679052@163.com>
2019-06-30 15:51:33 +0800

    liuhui

---
+++ src/main/java/com/hirun/app/task/HirunPlusXQLTEImportTask.java
@@ -0,0 +1,36 @@
+package com.hirun.app.task;
+
+import com.alibaba.fastjson.JSONObject;
+import com.most.core.app.service.invoker.ServiceInvoker;
+import com.most.core.pub.data.ServiceRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class HirunPlusXQLTEImportTask implements Job {
+
+    private static Logger log = LogManager.getLogger(HirunPlusXQLTEImportTask.class);
+
+    /**
+     *
+     *需求蓝图二信息拉取
+     *
+     */
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        Map parameter = new HashMap();
+        JSONObject jsonObject = new JSONObject(parameter);
+        ServiceRequest request = new ServiceRequest(jsonObject);
+        try {
+            ServiceInvoker.invoke("OperationCenter.custservice.XQLTEImportTaskService.hirunplusDataImport", request);
+        } catch (Exception e) {
+            log.error("数据导入后台任务执行失败：", e);
+        }
+    }
+}
