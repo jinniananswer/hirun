750d37638d591032d739bd3075f3c08c5aa24208
liuhui <lh936679052@163.com>
2019-08-19 23:54:14 +0800

    liuhui

--- src/main/java/com/hirun/app/dao/custservice/CustomerServiceDAO.java
+++ src/main/java/com/hirun/app/dao/custservice/CustomerServiceDAO.java
@@ -9,7 +9,6 @@ import com.most.core.pub.data.Record;
 import com.most.core.pub.data.RecordSet;
 import com.most.core.pub.tools.datastruct.ArrayTool;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.kafka.common.protocol.types.Field;
 
 import java.util.HashMap;
 import java.util.List;
@@ -281,7 +280,7 @@ public class CustomerServiceDAO extends StrongObjectDAO {
         sb.append("select * from ins_blueprint_action ");
         sb.append("where OPEN_ID=:OPEN_ID AND ACTION_CODE=:ACTION_CODE ");
         sb.append("AND REL_EMPLOYEE_ID = :EMPLOYEE_ID ");
-        sb.append("order by XQLTE_CREATE_TIME ");
+        sb.append("order by XQLTE_CREATE_TIME desc ");
         parameter.put("OPEN_ID", openid);
         parameter.put("ACTION_CODE", action_code);
         parameter.put("EMPLOYEE_ID", empId);
@@ -420,7 +419,7 @@ public class CustomerServiceDAO extends StrongObjectDAO {
 
         sb.append(" ) v");
         sb.append(" left join (select * from ins_scan_citycabin x where x.SCAN_ID in (select min(y.scan_id) from ins_scan_citycabin y group by y.PARTY_ID))  s on (s.PARTY_ID = v.PARTY_ID) ");
-        sb.append(" left join (select * from ins_blueprint_action r where r.BLUEPRINT_ACTION_ID in (select min(t.BLUEPRINT_ACTION_ID) from ins_blueprint_action t where t.ACTION_CODE='XQLTE' group by t.OPEN_ID, t.REL_EMPLOYEE_ID)) u on (u.OPEN_ID = v.OPEN_ID and u.REL_EMPLOYEE_ID = v.LINK_EMPLOYEE_ID) ");
+        sb.append(" left join (select * from ins_blueprint_action r where r.BLUEPRINT_ACTION_ID in (select max(t.BLUEPRINT_ACTION_ID) from ins_blueprint_action t where t.ACTION_CODE='XQLTE' group by t.OPEN_ID, t.REL_EMPLOYEE_ID)) u on (u.OPEN_ID = v.OPEN_ID and u.REL_EMPLOYEE_ID = v.LINK_EMPLOYEE_ID) ");
         sb.append(" left join (select k.PARTY_ID,count(1) visitcount from ins_party_visit k group by k.PARTY_ID ) j ON (v.PARTY_ID=j.PARTY_ID) ");
         sb.append(" order by v.create_date desc ");
 
