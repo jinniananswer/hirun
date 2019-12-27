e1c87637c11fac55004bb0e46041b4cfead80d4f
jinniananswer <38103152@qq.com>
2019-12-27 15:14:27 +0800

    --修改权限

--- most-app/src/main/java/com/most/core/app/session/RightsCollection.java
+++ most-app/src/main/java/com/most/core/app/session/RightsCollection.java
@@ -6,11 +6,15 @@ import com.most.core.pub.data.RecordSet;
 import com.most.core.pub.data.SessionEntity;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class RightsCollection {
 
-    private Map<String, String> rights;
+    private Set<String> rights;
+
+    private Set<String> menus;
 
     private RightsCollection() throws Exception{
         SessionEntity sessionEntity = SessionManager.getSession().getSessionEntity();
@@ -19,24 +23,59 @@ public class RightsCollection {
         Map<String, String> parameter = new HashMap<String, String>();
         parameter.put("USER_ID", userId);
 
+        GenericDAO insDAO = new GenericDAO("ins");
         StringBuilder sb = new StringBuilder();
-        sb.append("select b.func_id, b.func_code ");
-        sb.append("from ins.ins_user_func a, sys.sys_func b ");
-        sb.append("where b.func_id = a.func_id ");
-        sb.append("and b.status = '0' ");
-        sb.append("and a.status = '0' ");
-        sb.append("and a.user_id = :USER_ID");
+        sb.append("select a.user_id, a.role_id ");
+        sb.append("from ins_user_role a ");
+        sb.append("where user_id = :USER_ID ");
+        sb.append("and now() between start_date and end_date ");
+
+        RecordSet userRoles = insDAO.queryBySql(sb.toString(), parameter);
+        Record defaultRole = new Record();
+        defaultRole.put("ROLE_ID", "2");
+        defaultRole.put("USER_ID", userId);
+        if (userRoles == null) {
+            userRoles = new RecordSet();
+        }
+        userRoles.add(defaultRole);
+
+        String roles = "";
+        for (int i=0;i<userRoles.size();i++) {
+            roles += userRoles.get(i).get("ROLE_ID") + ",";
+        }
+        roles = roles.substring(0, roles.length() - 1);
+
+        StringBuilder funcRoleSql = new StringBuilder();
+        funcRoleSql.append("select b.func_id, b.func_code ");
+        funcRoleSql.append("from ins.ins_func_role a, sys.sys_func b ");
+        funcRoleSql.append("where b.func_id = a.func_id ");
+        funcRoleSql.append("and b.status = '0' ");
+        funcRoleSql.append("and a.status = '0' ");
+        funcRoleSql.append("and a.role_id in ("+roles+")");
 
         GenericDAO dao = new GenericDAO("all");
-        this.rights = new HashMap<String, String>();
-        RecordSet recordSet = dao.queryBySql(sb.toString(), parameter);
-        if(recordSet == null || recordSet.size() <= 0)
-            return;
+        this.rights = new HashSet<String>();
+        RecordSet funcRoles = dao.queryBySql(sb.toString(), new HashMap<>());
+        if (funcRoles != null) {
+            for (int i=0;i<funcRoles.size();i++) {
+                Record funcRole = funcRoles.get(i);
+                this.rights.add(funcRole.get("FUNC_CODE"));
+            }
+        }
 
-        int size = recordSet.size();
-        for(int i=0;i<size;i++){
-            Record record = recordSet.get(i);
-            rights.put(record.get("FUNC_ID"), record.get("FUNC_CODE"));
+        StringBuilder menuRoleSql = new StringBuilder();
+        menuRoleSql.append("select a.role_id, a.menu_id ");
+        menuRoleSql.append("from ins_menu_role a ");
+        menuRoleSql.append("where a.status = '0' ");
+        menuRoleSql.append("and role_id in ("+roles+") ");
+
+        RecordSet menuRoles = insDAO.queryBySql(menuRoleSql.toString(), new HashMap<>());
+        this.menus = new HashSet<String>();
+        if (menuRoles != null) {
+            for (int i=0;i<menuRoles.size();i++) {
+                Record menuRole = menuRoles.get(i);
+                this.menus.add(menuRole.get("MENU_ID"));
+            }
         }
     }
 
@@ -51,11 +90,11 @@ public class RightsCollection {
         }
     }
 
-    public boolean hasFuncId(String funcId){
-        return rights.containsKey(funcId);
+    public boolean hasFuncCode(String funcCode){
+        return rights.contains(funcCode);
     }
 
-    public boolean hasFuncCode(String funcCode){
-        return rights.containsValue(funcCode);
+    public boolean hasMenu(String menuId) {
+        return this.menus.contains(menuId);
     }
 }
