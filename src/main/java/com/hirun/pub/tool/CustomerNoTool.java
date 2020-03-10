33f74c67a0e0fb542d4e628003a3849658fb6447
liuhui <lh936679052@163.com>
2020-03-10 21:17:19 +0800

    【新增】新增漏提代码

---
+++ src/main/java/com/hirun/pub/tool/CustomerNoTool.java
@@ -0,0 +1,30 @@
+package com.hirun.pub.tool;
+
+import com.most.core.app.database.conn.ConnectionFactory;
+import com.most.core.app.database.dao.GenericDAO;
+import com.most.core.app.database.producer.ISqlProducer;
+import com.most.core.app.database.producer.SqlProducerFactory;
+import com.most.core.app.database.wrapper.ConnectionWrapper;
+import com.most.core.app.session.SessionManager;
+import com.most.core.pub.data.RecordSet;
+import com.most.core.pub.tools.time.TimeTool;
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.*;
+import java.util.HashMap;
+
+/**
+ * 获取customerNo
+ */
+public class CustomerNoTool {
+
+    public static String getCustomerNoSeq() throws Exception {
+        GenericDAO dao = new GenericDAO("sys");
+        String sql = "select nextval('SEQ_ID_DAY_CYCLE') as id";
+        RecordSet recordSet = dao.queryBySql(sql, new HashMap<>());
+        String id = recordSet.get(0).get("ID");
+        String strNextval = StringUtils.leftPad(String.valueOf(id), 4, '0');
+        String timestamp = TimeTool.now("yyyyMMdd");
+        return "KH"+timestamp+strNextval;
+    }
+}
