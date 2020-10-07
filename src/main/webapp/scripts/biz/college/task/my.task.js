aa85b0d1d7c624ba29273c8ec6f86dd78207f4cc
511942858@qq.com <qjackwocao!5>
2020-10-07 11:57:52 +0800

    【修改】 我的任务修改

--- src/main/webapp/scripts/biz/college/task/my.task.js
+++ src/main/webapp/scripts/biz/college/task/my.task.js
@@ -1,39 +1,168 @@
-require(['vue', 'vant', 'ajax', 'vant-select', 'page-title', 'redirect'], function (Vue, vant, ajax, vantSelect, pageTitle, redirect) {
+require(['vue', 'vant', 'ajax', 'vant-select', 'page-title', 'redirect', 'util'], function (Vue, vant, ajax, vantSelect, pageTitle, redirect, util) {
     let vm = new Vue({
         el: '#app',
         template: `
             <div>
                 <page-title title="我的任务"/>
-                <van-tabs v-model="active">
-                    <van-tab title="当前任务">
-                        <van-cell-group title="产品类">
-                            <van-cell title="家装知识学习任务" is-link label="描述信息" url="/biz/college/task/task_detail.html"/>
-                            <van-cell title="产品知识学习任务" is-link label="描述信息" />
-                        </van-cell-group>
-                    </van-tab>
-                    <van-tab title="明日任务">
-                        <van-cell-group title="产品类">
-                            <van-cell title="企业文化学习任务" is-link label="描述信息" />
-                            <van-cell title="客户服务学习任务" is-link label="描述信息" />
-                        </van-cell-group>
-                    </van-tab>
-                    <van-tab title="已完成">
-                        <van-empty image="search" description="还没有完成的任务哦" />
-                    </van-tab>
-                </van-tabs>
-                
+                <div style="margin-top:3.8rem">
+                    <van-tabs v-model="active">
+                        <van-tab title="当前任务">
+                            <template v-if="isTodayTask">
+                                <template v-if="isTodayCourseTask">
+                                    <van-cell-group title="课程任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.todayTask.courseTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId" />
+                                    </van-cell-group>
+                                </template>
+                                <template v-if="isTodayCoursewareTask">
+                                    <van-cell-group title="课件任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.todayTask.coursewareTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId"  />
+                                    </van-cell-group>
+                                </template>
+                                <template v-if="isTodayPracticeTask">
+                                    <van-cell-group title="实践任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.todayTask.practiceTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId" />
+                                    </van-cell-group>
+                                </template>
+                            </template>
+                            <template v-if="!isTodayTask">
+                                <van-empty image="search" description="今日没有需要完成的任务哦" />
+                            </template>
+                        </van-tab>
+                        
+                        <van-tab title="明日任务">
+                            <template v-if="isTomorrowTask">
+                                <template v-if="isTomorrowCourseTask">
+                                    <van-cell-group title="课程任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.tomorrowTask.courseTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId" />
+                                    </van-cell-group>
+                                </template>
+                                <template v-if="isTomorrowCoursewareTask">
+                                    <van-cell-group title="课件任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.tomorrowTask.coursewareTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId" />
+                                    </van-cell-group>
+                                </template>
+                                <template v-if="isTomorrowPracticeTask">
+                                    <van-cell-group title="实践任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.tomorrowTask.practiceTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId" />
+                                    </van-cell-group>
+                                </template>
+                            </template>
+                            <template v-if="!isTomorrowTask">
+                                <van-empty image="search" description="明日没有需要完成的任务哦" />
+                            </template>
+                        </van-tab>
+                        
+                        <van-tab title="已完成">
+                            <template v-if="isFinishTask">
+                                <template v-if="isFinishCourseTask">
+                                    <van-cell-group title="课程任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.finishTask.courseTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId" />
+                                    </van-cell-group>
+                                </template>
+                                <template v-if="isFinishCoursewareTask">
+                                    <van-cell-group title="课件任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.finishTask.coursewareTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId" />
+                                    </van-cell-group>
+                                </template>
+                                <template v-if="isFinishPracticeTask">
+                                    <van-cell-group title="实践任务">
+                                        <van-cell :title="item.taskName" v-for="item in myTaskInfo.finishTask.practiceTaskList" is-link :label="item.taskDesc" :url="'/biz/college/task/task_detail.html?taskId=' + item.taskId + '&isFinish=true'" />
+                                    </van-cell-group>
+                                </template>
+                            </template>
+                            <template v-if="!isFinishTask">
+                                <van-empty image="search" description="还没有完成的任务哦" />
+                            </template>
+                        </van-tab>
+                    </van-tabs>
+                </div>  
+                <bottom :active="2"></bottom>
             </div>`,
         data: function () {
             return {
                 value : '',
-                active : 0
+                active : 0,
+                myTaskInfo: {},
+                finishTaskInfo: [],
+                isTodayTask: false,
+                isTodayCourseTask: false,
+                isTodayCoursewareTask: false,
+                isTodayPracticeTask: false,
+                isTomorrowTask: false,
+                isTomorrowCourseTask: false,
+                isTomorrowCoursewareTask: false,
+                isTomorrowPracticeTask: false,
+                isFinishTask: false,
+                isFinishCourseTask: false,
+                isFinishCoursewareTask: false,
+                isFinishPracticeTask: false,
             }
         },
         methods: {
+            init : function() {
+                let that = this;
+                ajax.get('api/CollegeEmployeeTask/queryLoginEmployeeTaskInfo', '', function(data) {
+                    that.myTaskInfo = data;
+                    if (data != null && data != '{}'){
+                        if (data.todayTask != null && data.todayTask != '{}'){
+                            let todayTask = data.todayTask;
+                            if (todayTask.courseTaskList != null && todayTask.courseTaskList != '[]' && todayTask.courseTaskList.length > 0){
+                                that.isTodayCourseTask = true;
+                            }
+                            if (todayTask.coursewareTaskList != null && todayTask.coursewareTaskList != '[]' && todayTask.coursewareTaskList.length > 0){
+                                that.isTodayCoursewareTask = true;
+                            }
+                            if (todayTask.practiceTaskList != null && todayTask.practiceTaskList != '[]' && todayTask.practiceTaskList.length > 0){
+                                that.isTodayPracticeTask = true;
+                            }
+                            if (that.isTodayCourseTask || that.isTodayCoursewareTask || that.isTodayPracticeTask){
+                                that.isTodayTask = true;
+                            }
+                        }
+                        if(data.tomorrowTask != null && data.tomorrowTask != '{}'){
+                            let tomorrowTask = data.tomorrowTask;
+                            if (tomorrowTask.courseTaskList != null && tomorrowTask.courseTaskList != '[]' && tomorrowTask.courseTaskList.length > 0){
+                                that.isTomorrowCourseTask = true;
+                            }
+                            if (tomorrowTask.coursewareTaskList != null && tomorrowTask.coursewareTaskList != '[]' && tomorrowTask.coursewareTaskList.length > 0){
+                                that.isTomorrowCoursewareTask = true;
+                            }
+                            if (tomorrowTask.practiceTaskList != null && tomorrowTask.practiceTaskList != '[]' && tomorrowTask.practiceTaskList.length > 0){
+                                that.isTomorrowPracticeTask = true;
+                            }
+                            if (that.isTomorrowCourseTask || that.isTomorrowCoursewareTask || that.isTomorrowPracticeTask){
+                                that.isTomorrowTask = true;
+                            }
+                        }
+                        if (data.finishTask != null && data.finishTask != '{}'){
+                            let finishTask = data.finishTask;
+                            if (finishTask.courseTaskList != null && finishTask.courseTaskList != '[]' && finishTask.courseTaskList.length > 0){
+                                that.isFinishCourseTask = true;
+                            }
+                            if (finishTask.coursewareTaskList != null && finishTask.coursewareTaskList != '[]' && finishTask.coursewareTaskList.length > 0){
+                                that.isFinishCoursewareTask = true;
+                            }
+                            if (finishTask.practiceTaskList != null && finishTask.practiceTaskList != '[]' && finishTask.practiceTaskList.length > 0){
+                                that.isFinishPracticeTask = true;
+                            }
+                            if (that.isFinishCourseTask || that.isFinishCoursewareTask || that.isFinishPracticeTask){
+                                that.isFinishTask = true;
+                            }
+                        }
+                    }
 
+                });
+                // ajax.get('api/system/menu/listPhone', '', function(data) {
+                //     alert('aa');
+                //     alert(JSON.stringify(data));
+                // });
+            },
+            openTaskDetail: function (taskId) {
+                redirect.open('/biz/college/task/task_detail.html?taskId='+taskId, '任务详情');
+            }
         },
         mounted () {
-
+            this.init();
         }
     });
     return vm;
--- src/main/webapp/scripts/biz/college/task/task.detail.js
+++ src/main/webapp/scripts/biz/college/task/task.detail.js
@@ -1,74 +1,119 @@
-require(['vue', 'vant', 'ajax', 'vant-select', 'page-title', 'redirect'], function (Vue, vant, ajax, vantSelect, pageTitle, redirect) {
+require(['vue', 'vant', 'ajax', 'vant-select', 'page-title', 'redirect', 'util'], function (Vue, vant, ajax, vantSelect, pageTitle, redirect, util) {
     let vm = new Vue({
         el: '#app',
         template: `
             <div>
                 <page-title title="任务详情"/>
-                <van-cell style="background-color: #f8f8f8;color:#969799" :border="false" is-link title="家装知识学习任务" value="评分" @click="showEval"/>
-                <van-cell-group>
-                    <van-cell border="false" center="true">
-                        <template #title>
-                            <div class="van-multi-ellipsis">企业文化学习任务</div>
+                <div style="margin-top:3.8rem">
+                    <van-cell style="background-color: #f8f8f8;color:#969799" :border="false" is-link title="家装知识学习任务" value="评分" @click="showEval"/>
+                    <van-cell-group>
+                        <van-cell border="false" center="true">
+                            <template #title>
+                                <div class="van-multi-ellipsis">{{taskDetailInfo.taskName}}</div>
+                            </template>
+                            <template #right-icon v-if="taskDetailInfo.isDelayFlag">
+                                <van-tag type="danger">已延期</van-tag>
+                            </template>
+                            <template #label>
+                                <van-row>
+                                    <div class="van-multi-ellipsis--l2">{{taskDetailInfo.taskDesc}}</div>
+                                </van-row>
+                            </template>
+                        </van-cell>
+                        <template v-if="taskDetailInfo.studyType != '2'">
+                            <van-cell title="学习进度">
+                                <template #label>
+                                    <van-progress :percentage="taskDetailInfo.taskProgress" style="margin-top:1em"/>
+                                    <div class="van-multi-ellipsis--l2" style="margin-top:1em">
+                                         {{taskDetailInfo.studyStartDate}} ~ {{taskDetailInfo.sysdate}}
+                                    </div>
+                                </template>
+                            </van-cell>
                         </template>
-                        <template #right-icon>
-                            <van-tag type="danger">已延期</van-tag>
-                        </template>
-                        <template #label>
-                            <van-row>
-                                <div class="van-multi-ellipsis--l2">鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装鸿扬家装</div>
+                        <van-cell title="任务时间进度">
+                            <template #label>
+                                <van-progress :percentage="taskDetailInfo.taskTimeProgress" style="margin-top:1em"/>
+                                <div class="van-multi-ellipsis--l2" style="margin-top:1em">
+                                    {{taskDetailInfo.studyStartDate}} ~ {{taskDetailInfo.studyEndDate}}
+                                </div>
+                            </template>
+                        </van-cell>
+                    </van-cell-group>
+                    <template v-if="taskDetailInfo.taskStudyContentList != null && taskDetailInfo.taskStudyContentList != '[]' && taskDetailInfo.taskStudyContentList.length > 0">
+                        <van-cell-group title="学习课件">
+                            <van-cell :title="item.fileName" v-for="item in taskDetailInfo.taskStudyContentList" is-link @click="openFile(item.fileUrl)"/>
+                        </van-cell-group>
+                    </template>
+                    <template v-if="taskDetailInfo.studyType != '2'">
+                        <div style="margin-top:1em;margin-right:1em;margin-left:1em;margin-bottom:1em">
+                            <van-row :gutter="20">
+                                <van-col span="12">
+                                    <van-button :disabled="!taskDetailInfo.isExerciseFlag" @click="openFile(taskDetailInfo.isExerciseFlag)" type="primary" icon="plus" round block>我要练习</van-button>
+                                </van-col>
+                                <van-col span="12">
+                                    <van-button :disabled="!taskDetailInfo.isExamFlag"  @click="openFile(taskDetailInfo.isExamFlag)" type="danger" icon="fire-o" round block>我要考试</van-button>
+                                </van-col>
                             </van-row>
-                        </template>
-                    </van-cell>
-                    <van-cell title="任务时间">
-                        <template #label>
-                            <van-progress :percentage="10" style="margin-top:1em"/>
-                            <div class="van-multi-ellipsis--l2" style="margin-top:1em">
-                                2020-09-19~2020-09-30
-                            </div>
-                        </template>
-                    </van-cell>
-                </van-cell-group>
-                <van-cell-group title="学习课件">
-                    <van-cell title="学习课件1" is-link @click="openFile(1)"></van-cell>
-                    <van-cell title="学习课件2" is-link @click="openFile(2)"></van-cell>
-                    <van-cell title="学习课件3" is-link @click="openFile(3)"></van-cell>
-                    <van-cell title="学习课件4" is-link @click="openFile(4)"></van-cell>
-                </van-cell-group>
-                <div style="margin-top:1em;margin-right:1em;margin-left:1em;margin-bottom:1em">
-                    <van-row :gutter="20">
-                        <van-col span="12">
-                            <van-button type="primary" icon="plus" round block>我要练习</van-button>
-                        </van-col>
-                        <van-col span="12">
-                            <van-button type="danger" icon="fire-o" round block>我要考试</van-button>
-                        </van-col>
-                    </van-row>
-                </div>
-                <van-action-sheet v-model="show" title="评分">
-                    <div class="content">
-                        <van-field name="rate" label="课程难度">
-                            <template #input>
-                                <van-rate v-model="value" />
-                            </template>
-                        </van-field>
-                        <van-field name="rate" label="老师评分">
-                            <template #input>
-                                <van-rate v-model="value" />
-                            </template>
-                        </van-field>
-                        <div style="margin: 16px;">
-                            <van-button round block type="info" native-type="submit">
-                                提交
-                            </van-button>
                         </div>
-                    </div>
-                </van-action-sheet>
+                    </template>
+                    <template v-if="!isFinish" align="center">
+                        <template v-if="taskDetailInfo.studyType == '2'" align="center">
+                            <van-field
+                                  readonly
+                                  clickable
+                                  label="导师"
+                                  :value="tutor"
+                                  placeholder="选择导师"
+                                  @click="selectTutor"
+                                />
+                                <van-popup v-model="showTutorPicker" round position="bottom">
+                                  <van-picker
+                                    show-toolbar
+                                    :columns="selectTutorList"
+                                    @cancel="showTutorPicker = false"
+                                    @confirm="onConfirm"
+                                  />
+                                </van-popup>
+                            <template v-if="!taskDetailInfo.isSelectTutorFlag">
+                                <van-col span="12" align="center">
+                                    <van-button type="primary" icon="plus" round block>上传心得</van-button>
+                                </van-col>
+                            </template>
+                        </template>
+                    </template>
+                    <van-action-sheet v-model="show" title="评分">
+                        <div class="content">
+                            <van-field name="rate" label="课程难度">
+                                <template #input>
+                                    <van-rate v-model="value" />
+                                </template>
+                            </van-field>
+                            <van-field name="rate" label="老师评分">
+                                <template #input>
+                                    <van-rate v-model="value" />
+                                </template>
+                            </van-field>
+                            <div style="margin: 16px;">
+                                <van-button round block type="info" native-type="submit">
+                                    提交
+                                </van-button>
+                            </div>
+                        </div>
+                    </van-action-sheet>
+                </div>
+                <bottom :active="2"></bottom>
             </div>`,
         data: function () {
             return {
                 value : 2.5,
                 active : 0,
-                show : false
+                show : false,
+                taskId: util.getRequest("taskId"),
+                taskDetailInfo: {},
+                showTutorPicker: false,
+                selectTutorList: [],
+                tutor: '',
+                isFinish:  util.getRequest("isFinish")
             }
         },
         methods: {
@@ -76,12 +121,60 @@ require(['vue', 'vant', 'ajax', 'vant-select', 'page-title', 'redirect'], functi
                 this.show = true;
             },
 
-            openFile : function(fileId) {
-                alert(fileId);
+            openFile : function(fileUrl) {
+                alert(fileUrl);
+                redirect.open(fileUrl, '')
+            },
+
+            initTaskInfo: function () {
+                let that = this;
+                let taskId=that.taskId;
+                if(taskId=='undefined'){
+                    taskId=null;
+                }
+                ajax.get('/api/CollegeEmployeeTask/queryEmployTaskInfoByTaskId', {taskId:taskId}, function(data) {
+                    that.taskDetailInfo = data;
+                    that.tutor = data.selectTutor;
+                });
+            },
+            selectTutor: function () {
+                let that = this;
+                let isSelectTutorFlag = that.taskDetailInfo.isSelectTutorFlag;
+                if (!isSelectTutorFlag){
+                    vm.$toast.fail('导师已经选择，不能再次选择');
+                    return;
+                }
+                let taskId=that.taskId;
+                if(taskId=='undefined'){
+                    taskId=null;
+                }
+                ajax.get('/api/CollegeEmployeeTask/queryLoginEmployeeSelectTutor', '', function(data) {
+                    data.forEach((tutor) => {
+                        that.selectTutorList.push("[" + tutor.value + "]" + tutor.name);
+                    })
+                    that.showTutorPicker = true;
+                });
+            },
+            onCancel: function () {
+                this.showTutorPicker = false;
+            },
+            onConfirm: function (value) {
+                let that = this;
+                let taskId=that.taskId;
+                if(taskId=='undefined'){
+                    taskId=null;
+                }
+                let request = {};
+                request.taskId = taskId;
+                request.selectTutor = value;
+                ajax.post('api/CollegeEmployeeTaskTutor/addByTaskIdAndSelectTutor', request, function(responseData){
+                    that.showTutorPicker = false;
+                    that.initTaskInfo();
+                },null, true);
             }
         },
         mounted () {
-
+            this.initTaskInfo();
         }
     });
     return vm;
